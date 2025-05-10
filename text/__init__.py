4e7bf791a56cff8d8bd0e686326ed7be5d361e02
Abdelrahman Ramadan Fathi <153581888+Abdelrhman-Ramadan@users.noreply.github.com>
2025-05-10 18:57:41 +0300

    Update __init__.py

--- text/__init__.py
+++ text/__init__.py
@@ -1,5 +1,5 @@
 """ from https://github.com/keithito/tacotron """
-from text import cleaners
+# from text import cleaners
 from text.symbols import symbols
 
 
@@ -7,6 +7,10 @@ from text.symbols import symbols
 _symbol_to_id = {s: i for i, s in enumerate(symbols)}
 _id_to_symbol = {i: s for i, s in enumerate(symbols)}
 
+# phoneme_dict = {phone: idx for idx, phone in enumerate(phonemes)}
+# vocab = phoneme_dict
+# itos = list(vocab.keys())  # Index-to-string mapping
+# stoi = vocab  # String-to-index mapping
 
 def text_to_sequence(text, cleaner_names):
   '''Converts a string of text to a sequence of IDs corresponding to the symbols in the text.
@@ -16,14 +20,22 @@ def text_to_sequence(text, cleaner_names):
     Returns:
       List of integers corresponding to the symbols in the text
   '''
-  sequence = []
+  # sequence = []
 
-  clean_text = _clean_text(text, cleaner_names)
-  for symbol in clean_text:
-    symbol_id = _symbol_to_id[symbol]
-    sequence += [symbol_id]
-  return sequence
+  # clean_text = _clean_text(text, cleaner_names)
+  # for symbol in clean_text:
+  #   symbol_id = _symbol_to_id[symbol]
+  #   sequence += [symbol_id]
+  # return sequence
+  return encode(text)
 
+def encode(text):
+    # Convert a phoneme sequence into a list of indices
+    return [_symbol_to_id[phoneme] for phoneme in text.split() if phoneme in _symbol_to_id]
+
+def decode(tokens):
+    # Convert a list of indices back into a phoneme sequence
+    return ' '.join([_id_to_symbol[i] for i in tokens if i < len(_id_to_symbol)])
 
 def cleaned_text_to_sequence(cleaned_text):
   '''Converts a string of text to a sequence of IDs corresponding to the symbols in the text.
@@ -38,17 +50,18 @@ def cleaned_text_to_sequence(cleaned_text):
 
 def sequence_to_text(sequence):
   '''Converts a sequence of IDs back to a string'''
-  result = ''
-  for symbol_id in sequence:
-    s = _id_to_symbol[symbol_id]
-    result += s
-  return result
+  # result = ''
+  # for symbol_id in sequence:
+  #   s = _id_to_symbol[symbol_id]
+  #   result += s
+  # return result
+  return decode(sequence)
 
 
-def _clean_text(text, cleaner_names):
-  for name in cleaner_names:
-    cleaner = getattr(cleaners, name)
-    if not cleaner:
-      raise Exception('Unknown cleaner: %s' % name)
-    text = cleaner(text)
-  return text
+# def _clean_text(text, cleaner_names):
+#   for name in cleaner_names:
+#     cleaner = getattr(cleaners, name)
+#     if not cleaner:
+#       raise Exception('Unknown cleaner: %s' % name)
+#     text = cleaner(text)
+#   return text
